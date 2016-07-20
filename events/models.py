ce2416ceb3d63be58cd11f2f9811418457b87de3
vinay13 <NxtLife@Dhyanandras-iMac.local>
2016-07-21 01:01:30 +0530

    changes in events model

--- events/models.py
+++ events/models.py
@@ -1,15 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+#from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
+
+#@python_2_unicode_compatible
 class Event(models.Model):
 	id=models.AutoField(primary_key=True)
 	title=models.CharField(max_length=123,blank=True)
-	body=models.CharField(max_length=255,blank=True)
-	createdDate = models.DateTimeField(auto_now_add=True)
+	body=models.TextField()
+	createdDate = models.DateTimeField(db_index=True,auto_now_add=True)
 	event_date = models.DateField(auto_now=True)
-	event_time = models.CharField(max_length=50,blank=True)
+	start_time = models.TimeField(db_index=True,default="10:00")
+	end_time = models.TimeField(default="14:00")
 	event_type_id =models.IntegerField(blank=True)
 	event_standard = models.CharField(max_length=128,blank=True)
 
@@ -17,7 +21,7 @@ class Event(models.Model):
 
 
 	def __str__(self):
-		self.title
+		return self.title
 
 
 	
--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from events import views
 
 urlpatterns = patterns('',
     url(r'^events/',views.event_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$',views.event_detail)
+    url(r'^events/(?P<pk>[0-9]+)/$',views.event_detail)
 
 
 )
