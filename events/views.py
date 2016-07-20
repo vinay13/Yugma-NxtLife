8f7ae2ea4f674b7fab8e976a7e8d17a41a8118b1
vinay13 <NxtLife@Dhyanandras-iMac.local>
2016-07-21 00:20:02 +0530

    changed git username

--- events/views.py
+++ events/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-#from rest_framework	import status
 #from rest_framework.response import Response
 from events.models import Event
 from events.serializers import EventSerializer
