a51a33ceb42e2981579bff5a19b60e720e327304
vinay13 <aitvinay@gmail.com>
2016-07-21 14:24:57 +0530

    add poll_type in poll model

--- polls/models.py
+++ polls/models.py
@@ -4,13 +4,16 @@ from django.db import models
 
 # Create your models here.
 class Poll(models.Model):
+	POLL_TYPE =(
+		(1,'school'),
+		(2,'standard'),)
 	id=models.AutoField(primary_key=True)
 	createdAt=models.DateTimeField()
 	expiredAt=models.DateField()
 	is_active = models.BooleanField()
 	question= models.CharField(max_length=155)
 	user = models.CharField(max_length=144)
-	poll_type = models.BooleanField()
+	poll_type = models.ChoiceField(max_length=1,choices = POLL_TYPE)
 
 
 	def __str__(self):
@@ -20,7 +23,11 @@ class Poll(models.Model):
 
 class Options(models.Model):
 	
+	OPTIONS_CHOICES =((1,'single',
+					(2,'multiple'),
+		)
 	options = models.CharField(max_length=122)
+	options_type = models.ChoiceField(max_length=1,choices=OPTIONS_CHOICES)
 
 
 	def __str__(self):
