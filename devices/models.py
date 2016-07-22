03226084cc1fa0edbabd97c70c7579d52ff139dc
vinay13 <aitvinay@gmail.com>
2016-07-22 15:05:01 +0530

    devices model created

---
+++ devices/models.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+# Create your models here.
+# -*- coding: utf-8 -*-
+
+
+import datetime
+
+# Third Party Stuff
+from django.db import models
+from django.utils.timezone import now
+from uuidfield import UUIDField
+
+
+
+def expiry_time(expiry_mins=60):
+    return now() + datetime.timedelta(minutes=expiry_mins)
+
+
+class Device(models.Model):
+    uuid = UUIDField(version=1, hyphenate=True, unique=True, db_index=True)
+
+    # Verification
+    is_verified = models.BooleanField(default=False)
+    verification_code = models.IntegerField()
+    verification_code_sent_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Verification Code Sent At")
+    verification_code_expires_at = models.DateTimeField(
+        verbose_name="Verification Code Expires At",
+        default=expiry_time)
+    createdAt = models.DateTimeField(auto_now =True)
+
+    def __unicode__(self):
+        return u"uuid: {}, is_verified: {}".format(self.uuid,
+                                                   self.is_verified)
