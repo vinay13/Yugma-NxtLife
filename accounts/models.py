75e6714422c1b0d5775f485136c8c14eacfa0296
vinay13 <aitvinay@gmail.com>
2016-07-29 16:30:06 +0530

    added Account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.db.models.signals import post_save
+from base.models import TimeAuditModel
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.conf import settings
@@ -9,3 +10,21 @@ from django.conf import settings
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+
+
+
+"""
+
+class Account(TimeAuditModel):
+
+	user = models.OneToOneField(User)
+	contact_no = models.CharField(max_length =15 , blank =True)
+
+
+	def __str__(self):
+		return self.user.username
+
+
+"""
